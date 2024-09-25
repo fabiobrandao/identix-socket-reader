0d8812cd036abedfa8fbd3a7886fde1babd91eb9
Fábio Brandão <fabio@brainfonet.com>
2024-09-25 09:44:49 -0300

    Primeiro commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# identix-socket-reader
-Exemplo de conexão socket para leitores RFID Identix
+# Identix Socket Reader
+Simples exemplo de implementação de conexão Socket com leitores Identix.
+
+# Objetivo
+Este projeto apresenta de forma simples como implementar uma conexão socket com leitores Identix em C#.
+
+## Feedback
+
+Entre em <a href="http://www.fabiobrandao.net.br/" target="_blank">contato</a> para enviar seu feedback.
+
+Fábio Brandão - Fullstack Developer
---
+++ src/IdentixSocketReader/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO.Ports;
+using System.Threading;
+
+namespace IdentixSocketReader
+{
+    class Program
+    {
+        private static SerialPort serialPort = null;
+
+        static void Main(string[] args)
+        {
+            if (serialPort == null)
+            {
+                serialPort = new SerialPort("COM3", 115200, Parity.None, 8, StopBits.One);
+                serialPort.DataReceived += new SerialDataReceivedEventHandler(SerialReceivedData);
+            }
+
+            OpenSerialPort();
+            StartInventory();
+
+            Thread.Sleep(5000);
+
+            StopInventory();
+            CloseSerialPort();
+        }
+
+        private static void SerialReceivedData(object sender, SerialDataReceivedEventArgs e)
+        {
+            string data = ((SerialPort)sender).ReadExisting();
+            Console.WriteLine(data);
+        }
+
+        private static void OpenSerialPort()
+        {
+            try
+            {
+                if (!serialPort.IsOpen)
+                    serialPort.Open();
+
+                serialPort.DiscardInBuffer();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Could not open serial port connection. Exception: {ex.Message}");                
+            }            
+        }
+
+        private static void CloseSerialPort()
+        {
+            if (serialPort.IsOpen)
+                serialPort.Close();
+        }
+
+        private static void StartInventory()
+        {
+            if (!serialPort.IsOpen) return;
+            serialPort.Write("1\n");
+        }
+
+        private static void StopInventory()
+        {
+            if (!serialPort.IsOpen) return;
+            serialPort.Write("0\n");
+        }
+    }
+}
